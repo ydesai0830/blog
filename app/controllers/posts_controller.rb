11a6e0aa022dfb6d06f2c5e1e352ea475448286d
ydesai0830 <yash@mounza.com>
2014-12-30 18:43:20 -0800

    added verification and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,23 +5,48 @@ class PostsController < ApplicationController
 	end
 
 	def new
+    @post = Post.new
+  end
+ 
+  def create
+    @post = Post.new(post_params)
+ 
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-	end
+  def edit
+    @post = Post.find(params[:id])
+  end
+ 
+  def update
+    @post = Post.find(params[:id])
+ 
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to posts_path
+  end
 
 	def show
     	@post = Post.find(params[:id])
-  	end	
+  end	
 
-  	def create
-    	@post = Post.new(post_params)
- 
-    	@post.save
-    	redirect_to @post
-  	end
- 
-  	private
- 
-  	def post_params
-    	params.require(:post).permit(:title, :body)
-  	end
+	private
+  def post_params
+	 params.require(:post).permit(:title, :body)
+  end
 end
+  
+

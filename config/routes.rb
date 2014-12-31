4128ba2a2178cfd86cf17318336b55bb14384f44
ydesai0830 <yash@mounza.com>
2014-12-31 03:24:14 -0800

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+
   root "posts#index"
+
+  get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
